3af1ff31fd8a3311dfe6c0106616d834ae78cdf3
GlennWSo <sondellglenn@gmail.com>
2023-08-26 18:47:33 +0200

    test split_plane

--- src/bin/plane.rs
+++ src/bin/plane.rs
@@ -4,17 +4,12 @@ use halfedge::{Mesh, Plane};
 fn main() {
     let mut mesh1 = Mesh::unit_cube();
     mesh1.concave_triangulate();
+
     let mut mesh2 = mesh1.clone();
-    // let plane = Plane::new([0.5, 0.5, 0.5].into(), [0.0, 0.0, 1.0].into());
-    // mesh2.split_plane(plane);
-    mesh2.divide_edge(0);
-    // mesh2.concave_triangulate();
-    for (face_i, face) in mesh2.face_inds().enumerate() {
-        print!("face{}:", face_i);
-        for i in face {
-            print!(" {}", i);
-        }
-        println!();
-    }
+    let plane = Plane::new([0.5, 0.5, 0.5].into(), [1.0, 0.0, 0.0].into());
+    mesh2.split_plane(plane);
+    mesh2.concave_triangulate();
+    println!("{}", mesh2);
+    println!("{}", mesh2.pretty_face_vecs());
     show_wireframes(vec![mesh1.into(), mesh2.into()]);
 }
